cfac5d24b5dcf1d8b9813fb3b86d8f9b55273611
Irene K <linqwn@gmail.com>
2016-01-11 12:30:43 -0800

    removed old comments placed under post function for '/api/shows'

--- server.js
+++ server.js
@@ -172,6 +172,8 @@ app.post('/login', function(req, res){
       console.log('LOGGED IN!');
       //create a session
       createSession(req, res, user);
+      //log request
+      console.log(req);
       //redirect to main page
       res.redirect('/');
     } else {
@@ -205,99 +207,4 @@ app.post('/api/shows', function(req, response){
   //   res.redirect('/login');
   // }
 });
-
-
-
-
-///Last version -------------------
-
-
-//Modules removed in latest version
-// var cookieParser = require('cookie-parser');
-// var hat = require('hat');
-
-// //connect to local database
-// var database = 'mongodb://localhost/bugatti';
-// mongoose.connect(database, function(err, result){
-//   if(err) return err;
-//   console.log('Successfully connected to DB');
-// });
-
-// var port = process.env.PORT || 3000;
-// app.listen(port);
-// console.log('LISTENING TO PORT' + port);
-
-// //to see the requests in console
-// app.use(morgan('dev'));
-// app.use(bodyParser.json());
-// app.use(cookieParser());
-
-// //shall we require our routes here?
-// // require('./public/js/appRoutes')
-
-// app.use(express.static(__dirname + '/public'));
-
-// //POST for signup
-// app.post('/signup', function(req, res){
-//   var user = {
-//     //username is set to the request body's username
-//     username: req.body.username,
-//     //password is set to the hashed password syncronously
-//     password: bcrypt.hashSync(req.body.password, 10, function(err, hash){
-//       if(err) {throw (err)};
-//       return hash;
-//     }),
-//     //randomly generates a token by the hat module
-//     access_token: hat()
-//   };
-//   //mongoose .create()
-//   User.create(user, function(err){
-//     if (err) throw err;
-//     //maxAge = expiration time in ms & httpOnly is boolean to flag cookie for server
-//     res.cookie('access_token', user.access_token, {maxAge: 900000, httpOnly: true});
-//     res.send('success');
-//   });
-// });
-
-// POST for login
-// app.post('/login', function(req, res){
-//   var user_login = {
-//     username: req.body.username
-//   };
-
-//   //mongoose .findOne()
-//   User.findOne(user_login, function(err, user) {
-//     if (err) throw err;
-//     //ascyhronous bcrypt compare 
-//     bcrypt.compare(req.body.password, user.password, function(err, match){
-//       if (err) throw err;
-//       //check if the match is true
-//       if (match){
-//       //give use a token
-//       user.access_token = hat();
-//       //mongoose .save() the info to the db collection
-//       user.save();
-//       //create cookie session
-//       res.cookie('access_token', user.access_token, {maxAge: 900000, httpOnly: true});
-
-//       res.send('Logged in!');
-//     } else {
-//       res.send('Cannot log in!');
-//     }
-//     });
-//   });
-// });
-
-// //for when front end posts to api/shows - this is when the user selects a show to follow
-// app.post('/api/shows', function(req, res){
-//   // User.findOne(req.body.user, function(err, user){
-//   //   if(err) throw err;
-//     request('http://www.omdbapi.com/?i='+req.body.imdbID, function(error, res, body){
-//       if (error) throw error;
-//       console.log(body);
-//     });
-//     res.send('Show saved to the database');
-//   // });
-// });
-
 
